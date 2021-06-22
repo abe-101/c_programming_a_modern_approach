1153c720837f60fc7bee13352bb633c9b480a53f
abe-101 <slimhacker@pm.me>
2021-06-22 14:51:28 -0400

    add comments explaining program

--- pun.c
+++ pun.c
@@ -1,6 +1,12 @@
+/*
+ * Name: pun.c
+ * Purpose: Prints a bad pun.
+ * Auther: K. N. King
+ */
+
 #include <stdio.h>
 
-int main(void)
+int main(void)    //  Beginning of main program
 {
 	printf("To C, or not to C: ");
 	printf("that is the question.\n");
