faba217f682ac4f4fc42dcbdee63eee0671aac5b
abe-101 <slimhacker@pm.me>
2021-06-29 10:47:39 -0400

    organized repo

---
+++ ch02/Exercises/uninitialized.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	float c, d;
+
+	printf( "Two uninialized intgers: %d, %d\n", a, b);
+	printf( "Two uninialized floats: %d, %d\n", c, d);
+
+	return 0;
+}
+
---
+++ ch02/pun.c
@@ -0,0 +1,15 @@
+/*
+ * Name: pun.c
+ * Purpose: Prints a bad pun.
+ * Auther: K. N. King
+ */
+
+#include <stdio.h>
+
+int main(void)    //  Beginning of main program
+{
+	printf("To C, or not to C: ");
+	printf("that is the question.\n");
+	return 0 ;
+}
+
