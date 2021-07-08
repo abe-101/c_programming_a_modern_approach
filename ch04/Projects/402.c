3cb1799fe3cd068a3c43e9be02b5dd423b9100d4
abe-101 <slimhacker@pm.me>
2021-07-08 10:39:11 -0400

    Chapter 4

---
+++ ch04/Projects/402.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("Enter a three digit number: ");
+	scanf("%d", &n);
+	printf("The reversal is: %d%d%d\n", (n % 100) % 10, (n % 100) / 10, n / 100);
+
+	return 0;
+}
