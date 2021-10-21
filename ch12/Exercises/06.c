85b5a0a64fae23bcd3456999213d0d33656eff21
abe-101 <slimhacker@pm.me>
2021-10-21 15:20:36 -0400

    12/6 sum_array.c: Uses pointer arithmatic to sum array.

---
+++ ch12/Exercises/06.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define N 5
+
+int sum_array(const int a[], int n);
+
+int main(void)
+{
+    int a[5];
+    int *p;
+    printf("Enter %d digit array: ", N);
+    for (p = a; p < a + N; p++) {
+        scanf("%d", p);
+    }
+    printf("The total of the array: %d\n", sum_array(a, N));
+    return 0;
+}
+
+int sum_array(const int a[], int n)
+{
+    int sum = 0;
+    const int *p;
+    for(p = a; p < a + n; p++)
+        sum += *p;
+    return sum;
+}
