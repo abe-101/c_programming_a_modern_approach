725e6fd38f08f227d374ef0c6dccec7a589ec031
abe-101 <slimhacker@pm.me>
2021-10-24 10:49:04 -0400

    final polish

--- ch12/Exercises/08.c
+++ ch12/Exercises/08.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+#define N 10
+
+void store_zeros(int a[], int n);
+
+int main(void)
+{
+    int a[N] = {3, 4, 5, 6, 8, 3, 2, 5, 6, 7};
+    int *p;
+    for (p = a; p < a + N; p++)
+        printf("%d ", *p);
+    store_zeros(a, N);
+    printf("\n");
+    for (p = a; p < a + N; p++)
+        printf("%d ", *p);
+    printf("\n");
+
+    return 0;
+
+}
+
 void store_zeros(int a[], int n)
 {
     int *p;
