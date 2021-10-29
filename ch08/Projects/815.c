90146de4c56cfc2df012e1eec16d8d9c217754de
abe-101 <slimhacker@pm.me>
2021-10-29 10:28:52 -0400

    815.c: monor clean up

--- ch08/Projects/815.c
+++ ch08/Projects/815.c
@@ -2,30 +2,28 @@
 
 int main(void)
 {
-	int i, shift, length = 0;
-	char msg[80];
+    int i, shift, length = 0;
+    char msg[80];
 
-	printf("Enter message to be encrypted: ");
-	for (i = 0; (msg[i] = getchar()) != '\n' && i < 80; i++)
-		length++;
+    printf("Enter message to be encrypted: ");
+    for (i = 0; (msg[i] = getchar()) != '\n' && i < 80; i++)
+        length++;
 
-	printf("Enter shift amount (1-25): ");
-	scanf("%d", &shift);
+    printf("Enter shift amount (1-25): ");
+    scanf("%d", &shift);
 
-	printf("Encryped message: ");
+    printf("Encryped message: ");
 
-	for (i = 0; i < length; i++) {
-		if (msg[i] >= 'A' && msg[i] <= 'Z') 
-			msg[i] = ((msg[i] - 'A') + shift) % 26 + 'A';
-		else if ( msg[i] >= 'a' && msg[i] <= 'z')
-			msg[i] = ((msg[i] - 'a') + shift) % 26 + 'a';
-		putchar (msg[i]);
-	}
+    for (i = 0; i < length; i++) {
+        if (msg[i] >= 'A' && msg[i] <= 'Z') 
+            msg[i] = ((msg[i] - 'A') + shift) % 26 + 'A';
+        else if ( msg[i] >= 'a' && msg[i] <= 'z')
+            msg[i] = ((msg[i] - 'a') + shift) % 26 + 'a';
+        putchar (msg[i]);
+    }
 
-	//for (i = 0; i < length; i++)
-	//	printf("%c", msg[i]);
-	printf("\n");
+    printf("\n");
 
-	return 0;
+    return 0;
 }
 
