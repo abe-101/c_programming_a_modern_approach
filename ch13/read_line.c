416abc1ccbefd92980866a54f65fdf25d967cc67
abe-101 <slimhacker@pm.me>
2021-10-31 11:54:10 -0400

    read_line.c: function to store string from user into an array

---
+++ ch13/read_line.c
@@ -0,0 +1,23 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/*  (chapter 13, page 287) */
+
+#include <stdio.h>
+
+int read_lines(char str[], int n)
+{
+    int ch, i = 0;
+
+    while ((ch = getchar()) != '\n')
+        if (i < n)
+            str[i++] = ch;
+    str[i] = '\0';
+    return i;
+}
