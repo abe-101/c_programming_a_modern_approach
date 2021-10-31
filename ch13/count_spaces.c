0695aee845858be2f74ca6a19e58e6f5e3456a40
abe-101 <slimhacker@pm.me>
2021-10-31 11:56:17 -0400

    count_spaces.c: Function to count spaces in a string

---
+++ ch13/count_spaces.c
@@ -0,0 +1,21 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/*  (chapter 13, page 287) */
+#include <stdio.h>
+
+int count_spaces(const char s[])
+{
+    int count = 0, i;
+
+    for (i = 0; s[i] != '\0'; i++)
+        if (s[i] == ' ')
+            count++;
+    return count;
+}
