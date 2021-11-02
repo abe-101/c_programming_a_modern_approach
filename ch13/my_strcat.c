8dafb8222cdfc6cca547aedb92156f072da85679
abe-101 <slimhacker@pm.me>
2021-11-02 10:35:58 -0400

    my_strcat.c: condense version of mystrcat

--- ch13/my_strcat.c
+++ ch13/my_strcat.c
@@ -4,13 +4,9 @@ char *strcat(char *s1, const char *s2)
 {
     char *p = s1;
 
-    while (*p != '\0')
+    while (*p)
         p++;
-    while (*s2 != '\0') {
-        *p = *s2;
-        p++;
-        s2++;
-    }
-    *p = '\0';
+    while (*p++ = *s2++)
+        ;
     return s1;
 }
