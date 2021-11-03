7e308297000448416f21d856784e9dc37d30437b
abe-101 <slimhacker@pm.me>
2021-11-03 11:13:22 -0400

    13E/04d.c: Leaves behind characters that it doesn't have room to store

---
+++ ch13/Exercises/04d.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int read_line(char str[], int n);
+void print_line(char str[]);
+
+int main(void)
+{
+    char msg[20];
+    printf("Enter a message: ");
+    read_line(msg, 20);
+    print_line(msg);
+    return 0;
+}
+
+int read_line(char str[], int n)
+{
+    int ch, i = 0;
+
+    while ((str[i++] = getchar()) != '\n' && ch != EOF && i < n)
+        ;
+    str[i] = '\0';
+    return i;
+}
+
+void print_line(char str[])
+{
+    char *p;
+
+    for (p = str; *p != '\0'; p++)
+        printf("%c", *p);
+}
