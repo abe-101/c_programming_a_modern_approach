7bb6bc7dedd80a55b73ae764cac768e55d2be8f9
abe-101 <slimhacker@pm.me>
2021-11-03 11:05:15 -0400

    04a.c: made comlpete program

--- ch13/Exercises/04a.c
+++ ch13/Exercises/04a.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int read_lines(char str[], int n)
+int read_line(char str[], int n);
+void print_line(char str[]);
+
+int main(void)
+{
+    char msg[20];
+    printf("Enter a message: ");
+    read_line(msg, 20);
+    print_line(msg);
+    return 0;
+}
+
+int read_line(char str[], int n)
 {
     int ch, i = 0;
 
@@ -13,4 +25,12 @@ int read_lines(char str[], int n)
     }
     str[i] = '\0';
     return i;
+}
+
+void print_line(char str[])
+{
+    char *p;
+
+    for (p = str; *p != '\0'; p++)
+        printf("%c", *p);
 }
