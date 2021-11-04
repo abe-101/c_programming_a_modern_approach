8c4ed390015f725de996a6e41e2eca7d2fc6c2ae
abe-101 <slimhacker@pm.me>
2021-11-04 10:52:39 -0400

    13e/08.c

---
+++ ch13/Exercises/08.c
@@ -0,0 +1,23 @@
+/* 
+ * Chapter 13 Exercise 07.c: What will be the value of the 
+ * string str after the following statements have been executed?
+ * strcpy(str, "tire-bouchon");
+ * strcpy(&str[4], "d-or-wi");
+ * strcat(str, "red?");
+ * 
+ * By @abe_SlimHacker
+ *
+ * answer
+ * tired-or-wired? */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[20];
+    strcpy(str, "tire-bouchon");
+    strcpy(&str[4], "d-or-wi");
+    strcat(str, "red?");
+    printf("%s\n", str);
+}
