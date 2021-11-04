947eb90f54b337c8527cb0762edce1e442a10461
abe-101 <slimhacker@pm.me>
2021-11-04 10:52:03 -0400

    13E/07.c: minor fix

--- ch13/Exercises/07.c
+++ ch13/Exercises/07.c
@@ -1,1 +1,12 @@
-Statement d behaves diffrently. Because strcat appends a null character to the end of str.
+Chapter 13 Exercise 07.c Suppose that str is an array of
+characters. Which one of the following statements is not
+equivalent to the other three?
+
+(a) *str = 0;
+(b) str[0] = '\0';
+(c) strcpy(str, "");
+(d) strcat(str, "");
+
+By @abe_SlimHacker
+
+Answer: Statement d behaves diffrently. Because strcat appends a null character to the end of str.
