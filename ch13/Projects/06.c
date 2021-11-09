f0c048794746817d4d03d17cc88d1249fb7da938
abe-101 <slimhacker@pm.me>
2021-11-09 14:54:28 -0500

    13P/06.c: Improve planet.c to ignore case when comparing

---
+++ ch13/Projects/06.c
@@ -0,0 +1,49 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/*  (chapter 13, page 304) */
+
+/* Checks planet names */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_PLANETS 9
+
+int string_equal(const char *s, const char *t);
+
+int main(int argc, char *argv[])
+{
+    char *planets[] = {"Mercury", "Venus", "Earth",
+                       "Mars", "Jupiter", "Saturn",
+                       "Uranus", "Neptune", "Pluto"};
+    int i, j;
+
+    for (i = 1; i < argc; i++) {
+        for (j = 0; j < NUM_PLANETS; j++)
+            if (string_equal(argv[i], planets[j])) {
+                printf("%s is planet %d\n", argv[i], j + 1);
+                break;
+            }
+        if (j == NUM_PLANETS)
+            printf("%s is not a planet\n", argv[i]);
+    }
+    return 0;
+}
+
+int string_equal(const char *s, const char *t)
+{
+    int i;
+
+    for (i = 0; toupper(s[i]) == toupper(t[i]); i++)
+        if (s[i] == '\0')
+            return 1;
+    return 0;
+}
