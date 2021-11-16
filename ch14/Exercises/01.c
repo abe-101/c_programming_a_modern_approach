47f7d1a3602a708a96e2245b4c9063e63c9af920
abe-101 <slimhacker@pm.me>
2021-11-16 10:14:46 -0500

    14E/01.c: Add parameterized macro that computes 1 if the product of x and y is less than 100

--- ch14/Exercises/01.c
+++ ch14/Exercises/01.c
@@ -2,9 +2,15 @@
 
 #define CUBE(x) ((x)*(x)*(x))
 #define REMAINDER(n) ((n)%(4))
+#define IS_LESS(x,y) ((x)+(y)<(100)?(1):(0))
 
 int main(void)
 {
     printf("The cude of 4 is: %d\n", CUBE(4));
     printf("The Remainder of 18 / 4: %d\n", REMAINDER(18));
+    if (IS_LESS(37,64))
+        printf("The sum of 34 and 64 is less than 100\n");
+    else
+            printf("The sum of 37 and 64 is not less than 100\n");
+
 }
