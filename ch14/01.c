71de85c5c1be2dae9cd4ad4e34d4fdd1bb35ab54
abe-101 <slimhacker@pm.me>
2021-11-16 09:54:39 -0500

    14E/01.c: Add parameterized macro to compute the remainder when n is divided by 4

--- ch14/01.c
+++ ch14/01.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 #define CUBE(x) ((x)*(x)*(x))
+#define REMAINDER(n) ((n)%(4))
 
 int main(void)
 {
     printf("The cude of 4 is: %d\n", CUBE(4));
+    printf("The Remainder of 18 / 4: %d\n", REMAINDER(18));
 }
