87555e17feb9fa8f77ec82dec2a235e3d1b6e89b
abe-101 <slimhacker@pm.me>
2021-11-21 14:41:04 -0500

    word.c: Condens read_char function

--- ch15/justify/word.c
+++ ch15/justify/word.c
@@ -5,9 +5,7 @@ int read_char(void)
 {
     int ch = getchar();
 
-    if (ch == '\n' || ch == '\t')
-        return ' ';
-    return ch;
+    return (ch == '\n' || ch == '\t') ? ' ' : ch;
 }
 
 void read_word(char *word, int len)
