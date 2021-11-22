4160e9221682582a205946ee254f34a17c765b8f
abe-101 <slimhacker@pm.me>
2021-11-22 15:27:58 -0500

    Modified justify by having the read_word function store the * charachter in a word thats too long

---
+++ ch15/Projects/02/word.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "word.h"
+
+int read_char(void)
+{
+    int ch = getchar();
+
+    return (ch == '\n' || ch == '\t') ? ' ' : ch;
+}
+
+int read_word(char *word, int len)
+{
+    int ch, pos = 0;
+
+    while ((ch = read_char()) == ' ')
+        ;
+    while (ch != ' ' && ch != EOF) {
+        if (pos < len -1)
+            word[pos++] = ch;
+        else if (pos == len -1)
+            word[pos++] = '*';
+        ch = read_char();
+    }
+    word[pos] = '\0';
+    return pos;
+}
