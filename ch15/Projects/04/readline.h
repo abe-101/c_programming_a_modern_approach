daf189d506e84b779d466f0e740caec5ee6a0a42
abe-101 <slimhacker@pm.me>
2021-11-23 10:54:57 -0500

    add prototype

---
+++ ch15/Projects/04/readline.h
@@ -0,0 +1,11 @@
+#ifndef READLINE_H
+#define READLINE_H
+
+// Prototype
+
+/*******************************************************
+ * read_line: Reads a line of text                     *
+ *******************************************************/
+int read_line(char str[], int n);
+
+#endif
