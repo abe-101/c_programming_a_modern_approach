bbc4d66f299821d54c04f815a9f181f4cc7a222f
abe-101 <slimhacker@pm.me>
2021-11-30 10:34:54 -0500

    add function that stores its two aurguments in a complex structure

--- ch16/Exercises/03.c
+++ ch16/Exercises/03.c
@@ -3,7 +3,16 @@
 struct complex {
     double real;
     double imaginary;
-    }
+    };
+
+struct complex make_complex(double real, double imaginary)
+{
+    struct complex newcomplex;
+    newcomplex.real = real;
+    newcomplex.imaginary = imaginary;
+
+    return newcomplex;
+}
 
 int main(void)
 {
