79ed6686f9df829bacebb3d8b1b51a0bd6336b4b
abe-101 <slimhacker@pm.me>
2021-11-30 10:13:27 -0500

    add statement that copy the members of c2 into c1

--- ch16/Exercises/02.c
+++ ch16/Exercises/02.c
@@ -8,6 +8,8 @@ int main(void)
     } c1 = {0.0, 1.0},
       c2 = {1.0, 0.0},
       c3;
+
+    c1 = c2;
     
     printf("Struct c1: real = %.1f, imaginary = %.1f\n", c1.real, c1.imaginary);
     printf("Struct c2: real = %.1f, imaginary = %.1f\n", c2.real, c2.imaginary);
