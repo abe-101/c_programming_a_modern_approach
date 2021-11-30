1c79cb2632ef07775045ef6012ff986e9bf6ca12
abe-101 <slimhacker@pm.me>
2021-11-30 10:15:13 -0500

    add statement that add the corresponding members of c1 and c2 storing the results in c3

--- ch16/Exercises/02.c
+++ ch16/Exercises/02.c
@@ -11,6 +11,10 @@ int main(void)
 
     c1 = c2;
     
+    c3.real = c1.real + c2.real;
+    c3.imaginary = c1.imaginary + c2.imaginary;
+    
+
     printf("Struct c1: real = %.1f, imaginary = %.1f\n", c1.real, c1.imaginary);
     printf("Struct c2: real = %.1f, imaginary = %.1f\n", c2.real, c2.imaginary);
     printf("Struct c3: real = %.1f, imaginary = %.1f\n", c3.real, c3.imaginary);
