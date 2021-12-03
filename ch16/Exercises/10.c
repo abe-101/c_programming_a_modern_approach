3c2f9a1151b49a2c1cbcbf862884f598b22aa1b9
abe-101 <slimhacker@pm.me>
2021-12-03 09:52:35 -0500

    compute_center: compute the center of rectangle

--- ch16/Exercises/10.c
+++ ch16/Exercises/10.c
@@ -7,6 +7,8 @@ struct rectangle { struct point upper_left, lower_right; };
 int compute_width(struct rectangle r);
 int compute_height(struct rectangle r);
 int compute_area(struct rectangle r);
+struct point compute_center(struct rectangle r);
+
 
 int main(void)
 {
@@ -14,6 +16,9 @@ int main(void)
     struct point lower_right = {15, 10};
     struct rectangle r1 = {upper_left, lower_right};
     printf("Area: %d\n", compute_area(r1));
+    struct point center = compute_center(r1);
+    printf("Center: %d,%d\n", center.x, center.y);
+
     return 0;
 }
 
@@ -33,4 +38,11 @@ int compute_area(struct rectangle r)
     return compute_width(r) * compute_height(r);
 }
 
+struct point compute_center(struct rectangle r)
+{
+    struct point center;
+    center.x = r.upper_left.x + compute_width(r) / 2;
+    center.y = r.upper_left.y + compute_height(r) / 2;
+    return center;
+}
 
