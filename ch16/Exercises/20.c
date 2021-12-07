2719a7fd9d2a7568b1725c26819ac5ef4de989d0
abe-101 <slimhacker@pm.me>
2021-12-07 11:39:44 -0500

    20.c: direction

---
+++ ch16/Exercises/20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+enum {NORTH, SOUTH, EAST, WEST} direction;
+int main(void)
+{
+    int x = 0, y = 0;
+
+    switch (direction)
+    {
+        case EAST:  x++;
+                    break;
+        case WEST:  x--;
+                    break;
+        case SOUTH: y++;
+                    break;
+        case NORTH: y--;
+                    break;
+    }
+}
