9b83df1fa44737127cef04f17b2fe35d00bfd57a
abe-101 <slimhacker@pm.me>
2021-12-11 21:58:56 -0500

    part database (linked list version)

--- ch17/inventory2/inventory2.c
+++ ch17/inventory2/inventory2.c
@@ -53,51 +53,68 @@ int main(void)
     }
 } 
 
- /********************************************************
- * find_part: Looks up a part number in the inventory    * 
- *            list. Returns a pointer to the node*
- *            number is found; otherwise, return -1.     * 
- *********************************************************/
-int find_part(int number)
+ /*******************************************************
+ * find_part: Looks up a part number in the inventory   * 
+ *            list. Returns a pointer to the node       *
+ *            containing the part number; if the part   *
+ *            number is not found, returns NULL.        *
+ ********************************************************/
+struct part *find_part(int number)
 {
-    int i;
+    struct part *p;
 
-    for (i = 0; i < num_parts; i++)
-        if (inventory[i].number == number)
-            return i;
-    return -1;
+    for (p = inventory;
+         p != NULL && number > p->number;
+         p = p->next)
+        ;
+    if (p != NULL && number == p->number)
+        return p;
+    return NULL;
 }
 
 
-/*********************************************************
- * insert: Promts the user for information about a new   *
- *         part and then inserts the part into the       *
- *         database. Prints an error message and returns *
- *         prematurely if the part already exists or the *
- *         database if full.i                            *
- *********************************************************/
+/**********************************************************
+ * insert: Promts the user for information about a new    *
+ *         part and then inserts the part into the        *
+ *         inventory list; the list remains sorted by     *
+ *         part number. Prints an error message and       *
+ *         returns prematurely if the part already exists *
+ *         or space could not be allocated for the part   *
+ **********************************************************/
 void insert(void)
 {
-    int part_number;
+    struct part *cur, *prev, *new_node;
 
-    if (num_parts == MAX_PARTS) {
-        printf("Database is full; can't add more parts.\n");
+    new_node = malloc(sizeof(struct part));
+    if (new_node == NULL) {
+        printf("Database id full; can't add more parts.\n");
         return;
     }
+
     printf("Enter part number: ");
-    scanf("%d", &part_number);
+    scanf("%d", &new_node->number);
 
-    if (find_part(part_number) >= 0) {
+    for (cur = inventory, prev = NULL;
+         cur != NULL && new_node->number > cur->number;
+         prev = cur, cur = cur->next)
+        ;
+    if (cur != NULL && new_node->number == cur->number) {
         printf("Part already exists.\n");
+        free(new_node);
         return;
     }
 
-    inventory[num_parts].number = part_number;
     printf("Enter part name: ");
-    read_line(inventory[num_parts].name, NAME_LEN);
+    read_line(new_node->name, NAME_LEN);
     printf("Enter quantity on hand: ");
-    scanf("%d", &inventory[num_parts].on_hand);
-    num_parts++;
+    scanf("%d", &new_node->on_hand);
+
+    new_node->next = cur;
+    if (prev == NULL)
+        inventory = new_node;
+    else
+        prev->next = new_node;
+    
 }
 
  /***********************************************************
@@ -108,35 +125,38 @@ void insert(void)
  ************************************************************/
 void search(void)
 {
-    int i, number;
+    int number;
+    struct part *p;
 
     printf("Enter part number: ");
     scanf("%d", &number);
-    i = find_part(number);
-    if (i >= 0) {
-        printf("Part name: %s\n", inventory[i].name);
-        printf("Quantity on hand: %d\n", inventory[i].on_hand);
+    p = find_part(number);
+    if (p != NULL) {
+        printf("Part name: %s\n", p->name);
+        printf("Quantity on hand: %d\n", p->on_hand);
     } else
         printf("Part not found.\n");
 }
 
  /***********************************************************
  * update: Promts the user to enter a part number.          *
- *         Prints an erroe message if the part doesn't      *
+ *         Prints an error message if the part doesn't      *
  *         exist; otherwise, prompts the user to enter      *
+ *         change in quantity on hand and update the        *
  *         database.                                        *
  ************************************************************/
 void update(void)
 {
-    int i, number, change;
+    int number, change;
+    struct part *p;
 
     printf("Enter part number: ");
     scanf("%d", &number);
-    i = find_part(number);
-    if (i >= 0) {
+    p = find_part(number);
+    if (p != NULL) {
         printf("Enter change in quantity on hand: ");
         scanf("%d", &change);
-        inventory[i].on_hand += change;
+        p->on_hand += change;
     } else
         printf("Part not found.\n");
 }
@@ -145,18 +165,17 @@ void update(void)
  /***********************************************************
  * print: Prints a listing of all parts in the database,    *
  *        showing the part number, part name, and           *
- *        quantity on hand. Parts are printed on the        *
- *        order in which they were entered into the         *
- *        database.                                         *
+ *        quantity on hand. Part numbers will appear in     *
+ *        ascending order.                                  *
  ************************************************************/
 void print(void)
 {
-    int i;
+    struct part *p;
 
     printf("Part Number    Part Name                "
            "Quantity on Hand\n");
-    for (i = 0; i < num_parts; i++)
-    printf("%7d        %-25s%11d\n", inventory[i].number,
-            inventory[i].name, inventory[i].on_hand);
+    for (p = inventory; p != NULL; p = p->next)
+    printf("%7d        %-25s%11d\n", p->number,
+           p->name, p->on_hand);
 }
 
