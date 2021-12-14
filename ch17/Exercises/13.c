400f9ba036c1f72229bf0d70909fa319ae369c8b
Abe <abe@habet.dev>
2021-12-14 10:28:38 -0500

    insert_into_ordered_list:
    special attention need when list is empty and when inserting into first link

--- ch17/Exercises/13.c
+++ ch17/Exercises/13.c
@@ -6,27 +6,31 @@ struct node {
     struct node *next;
 };
 
-struct node *insert_into_ordered_list(struct node *lists,
+struct node *insert_into_ordered_list(struct node *list,
                                       struct node *new_node)
 {
+    // Special attention is needed When either cur is null (empty list) or prev is null (first node in list).
+    
+    // Case where list is empty
     struct node *cur = list, *prev = NULL;
+    if (list == NULL) {
+        return new_node;
+    }
+
+    // travers though list
     while (cur->value <= new_node->value) {
         prev = cur;
         cur = cur->next;
     }
-
-    // Special attention is needed When either cur is null (empty list) or prev is null (first node in list).
-
-    if (list == NULL) {
-        return new_node;        /* Case where list is empty */
-    }
-    else if (prev == NULL) {
+    // Case where inserting before first node
+       if (prev == NULL) {
         list->next = new_node;
-        new_node->next = cur;   /* Case where inserting before first node */
+        new_node->next = cur; 
     }
+    
+    //All other cases
     else {
         prev->next = new_node;
-        new_node->next = cur;   /* All other cases */
+        new_node->next = cur;  
     }
 }
-
