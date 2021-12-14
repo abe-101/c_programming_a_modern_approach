ec648a1a126552a42be1c58438b787e3195deb1f
Abe <abe@habet.dev>
2021-12-14 12:13:01 -0500

    19.c

---
+++ ch17/Exercises/19.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct {
+    char *cmd_name;
+    void (*cmd_pointer)(void);
+} file_cmd[] =
+    {{"new",       new_cmd},
+     {"open",      open_cmd},
+     {"close",     close_cmd},
+     {"close all", close_all_cmd},
+     {"save as",   save_as_cmd},
+     {"save all",  save_all_cmd},
+     {"print",     print_cmd},
+     {"exit",      exit_cmd}
+    };
+
+void run_command(char *str) {
+    int i;
+    for (i = 0; i < sizeof(file_cmd)/sizeof(file_cmd[0]); i++)
+        if (strcmp(str, file_cmd[i].cmd_name) == 0)
+            return (*file_cmd[i].cmd_pointer)();
+    return;
+}
